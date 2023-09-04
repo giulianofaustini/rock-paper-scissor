9d163fcdde45e4ebd7c4cb84f858492239b1b158
Giuliano Faustini <giuliperus@gmail.com>
2023-09-04 07:36:33 +0300

    first commit

---
+++ src/components/Choices.js
@@ -0,0 +1,16 @@
+
+
+import React from 'react';
+
+
+const Choices = () => [
+
+{ name: "rock" , emoji: " 🪨 " },
+{ name: "paper" , emoji: " 🧻 " },
+{ name: "scissors", emoji: " ✂️  " }
+
+
+]
+ 
+
+export default Choices;
